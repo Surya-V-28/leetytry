08168bea2791e25b03843a7acf99ffeef9b340ec
Surya-V-28 <suryaarmy231@gmail.com>
2023-09-09 18:52:32 +0530

    Daily Temperature Stack

---
+++ stacks/DailyTemperature739.java
@@ -0,0 +1,16 @@
+
+
+
+import java.util.*;
+
+
+
+public class DailyTemperature739 {
+    public static void main(String args[]){
+        System.out.print("Working");
+        int[] nums = {73,74,75,71,69,72,76,73};
+        int[] nums2 = new int[nums.length];
+        nums2[nums.length-1] = 0;
+        int index = 1,i; 
+    }
+}
