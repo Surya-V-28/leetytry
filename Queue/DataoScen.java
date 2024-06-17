7b571db9aef4cd58a57b7b9bc2a7af34553b288a
Surya-V-28 <suryaarmy231@gmail.com>
2024-06-17 22:16:45 +0530

    the Right Side of the tree is travsel added using level Order

--- Queue/DataoScen.java
+++ Queue/DataoScen.java
@@ -1,25 +1,42 @@
 
-
+import java.util.*;
 public class DataoScen {
     public static void main(String args[]) {
 
 
         System.out.println("Wokring");
-        senate = "RD";
-        Queue<Character> radiant = new LinkedList<Character>();
-        Queue<Character> dire = new LinkedList<Character>();
-
+        String senate = "RDD";
+        int total = senate.length();
+        Queue<Integer> radiant = new LinkedList<Integer>();
+        Queue<Integer> dire = new LinkedList<Integer>();
 
         for(int i=0;i<senate.length();i++){
             if(senate.charAt(i)=='R') {
-             radiant.add(senate.charAt(i));
+             radiant.add(i);
             }
             else{
-                dire.add(senate.charAt(i));
+                dire.add(i);
             }
         }
         while(!radiant.isEmpty() && !dire.isEmpty()) {
-            
+              if(radiant.peek()<dire.peek()) {
+                dire.remove();
+                int ans = radiant.remove();
+                total = total+1;
+                radiant.add(total);  
+              }
+              else {
+                radiant.remove();
+                int ans =  dire.remove();
+                total = total+1;
+                dire.add(total);  
+              }
+        }
+        if(radiant.isEmpty()){
+            System.out.println("R is weak");
+        }
+        if(dire.isEmpty()){
+            System.out.println("D is weak");
         }
     }
 }
---
+++ trees/RightSideofBinaryTree.java
@@ -0,0 +1,31 @@
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    List<Integer> arr = new ArrayList<>();
+   public void InOrder(TreeNode node,int level){
+       if(node==null) return;
+       if(level == arr.size()){
+        arr.add(node.val);
+       }
+       InOrder(node.right,level+1);
+       InOrder(node.left,level+1);
+   }
+   public List<Integer> rightSideView(TreeNode root) {
+       InOrder(root,0);
+       return  arr;
+   }
+}
