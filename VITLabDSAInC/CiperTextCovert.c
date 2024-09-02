e5f4f4c4416dbaa42dda9c7466b85640587d50d1
Surya-V-28 <suryaarmy231@gmail.com>
2024-09-02 06:21:23 +0530

    completed the DA-01 VIT- Labs

--- VITLabDSAInC/CiperTextCovert.c
+++ VITLabDSAInC/CiperTextCovert.c
@@ -2,13 +2,17 @@
 #define max 100
 int main() {
     char  str[max];
+    char ch;
     char newStr[max];
     int turn=0;
     do {
-    printf("Enter the text to convert to ciper \n");
+    printf("Enter the text to convert to ciper in Upper Case \n");
     scanf("%s", str);
-    printf("Enter the number of the shift you wanna do \n");
+    printf("Enter the number of the shift you wanna do for[0-25] \n");
     scanf("%d", &turn);
+    if(!(turn>=0 && turn <=25)){
+        printf("then entered number is not in the range we convert to range using %d mod 25 is %d \n", turn, turn%26);
+    }
     int i=0;
     while(str[i] !='\0'){
         if(str[i]>='a' && str[i]<='z') {
@@ -22,8 +26,11 @@ int main() {
          }
         i++;
     }
-    printf("the entered string is %s", newStr);
-    
-    }while(ch=='y')
+     newStr[i] = '\0'; 
+    printf("the Converted ciper string is %s \n", newStr);
+    printf("Do you wish to continue yes = y / No = N \n");
+    getchar();  
+    scanf("%c",&ch);
+    }while(ch=='y');
     return 0;
 }
