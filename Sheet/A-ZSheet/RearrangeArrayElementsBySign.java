022b0e4444b73544791ec11b43597e94e6498b2a
Surya-V-28 <suryaarmy231@gmail.com>
2024-10-21 08:57:35 +0530

    RearrangeArrayElementsBySign added Sheet Problems

---
+++ Sheet/A-ZSheet/RearrangeArrayElementsBySign.java
@@ -0,0 +1,46 @@
+
+
+public class RearrangeArrayElementsBySign {
+
+    public static void GoodMethod(int[] arrs) {
+        int pos =0;
+        int neg =1; 
+        int[] nums = new int[arrs.length];
+
+        for(int i=0;i<arrs.length;i++){
+            if(arrs[i]>0){
+                nums[pos] = arrs[i];
+                pos= pos+2;
+            } else {
+                nums[neg] = arrs[i];
+                neg= neg+2;
+            }
+        }
+    }
+
+    public static void main(String args[]){
+        int[] posArr = new int[nums.length+1/2];
+        int[] negArr = new int[nums.length+1/2];
+        int pos=0, neg =0;
+
+        for(int i=0;i<nums.length;i++) {
+            if(nums[i]>0){
+                posArr[pos++]=nums[i];
+            } else {
+                negArr[neg++]=nums[i];
+            }
+        }
+        int counter=0;
+        int i=0;
+        while(i<nums.length/2){
+            nums[counter++] = posArr[i];
+            nums[counter++] =negArr[i];
+            i++;
+        }
+
+        return nums; 
+    }
+    
+}
+
+
