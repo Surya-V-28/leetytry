9e17e112a891fb5a97aad0b495c665c68ccf2b0d
Surya-V-28 <suryaarmy231@gmail.com>
2025-02-15 22:52:09 +0530

    added the back traccking problems

---
+++ backtracking/FindPunishMentNumber.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isTotal(int n,int currSub,String sqrt){
+        if(currSub==n && sqrt.isEmpty()) return true;
+        if(currSub>n) return false;
+        for (int i=0;i<sqrt.length();i++){
+            String left  = sqrt.substring(0,i+1);
+            String right = sqrt.substring(i+1);
+            if(isTotal(n,currSub +Integer.parseInt(left),right)){
+                return true;
+            }
+
+        }
+        
+        return false;
+    }
+    public int punishmentNumber(int n) {
+       int ans =1;
+       if (n<9) return ans;
+       for(int i =9;i<=n;i++){
+          if(isTotal(i, 0,String.valueOf(i*i))){
+            ans+=i*i;
+          }
+       }
+       return ans;
+    }
+}
