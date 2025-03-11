4a950ff7c8697a8c5b0b15001dd321f94f966700
Surya-V-28 <suryaarmy231@gmail.com>
2025-03-11 08:49:15 +0530

    adding the new template for Slinding Window

--- LastMinPrep/SlindingWindowArrays.java
+++ LastMinPrep/SlindingWindowArrays.java
@@ -114,9 +114,50 @@ public class SlindingWindowArrays {
         // 2. Finding the count of the Subarrays with given Sum = k
         // 3. Find the Longest Subarray size with given distant characters in it;
 
-
+    
 
     /// Number of Substrings Containing All Three Characters or the give characters in Strings
 
+    }
+
+
+    private boolean isVowel(char c) {
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+    }
+
+    private long atLeastK(String word, int k) {
+        int n = word.length();
+        long ans = 0;
+        int consonants = 0;
+        int left = 0;
+        HashMap<Character, Integer> vowelMap = new HashMap<>();
+
+        for (int right = 0; right < n; right++) {
+            char c = word.charAt(right);
+            if (isVowel(c)) {
+                vowelMap.put(c, vowelMap.getOrDefault(c, 0) + 1);
+            } else {
+                consonants++;
+            }
+
+            while (vowelMap.size() == 5 && consonants >= k) {
+                ans += n - right;
+                char leftChar = word.charAt(left);
+                if (isVowel(leftChar)) {
+                    vowelMap.put(leftChar, vowelMap.get(leftChar) - 1);
+                    if (vowelMap.get(leftChar) == 0) {
+                        vowelMap.remove(leftChar);
+                    }
+                } else {
+                    consonants--;
+                }
+                left++;
+            }
+        }
+        return ans;
+    }
+
+    public long countOfSubstrings(String word, int k) {
+        return atLeastK(word, k) - atLeastK(word, k + 1);
     }
 }
